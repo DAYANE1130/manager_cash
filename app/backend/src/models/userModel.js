3d9ae2e1187804c80960927658e6343998ad8832
Dayane <dayanebmartins@gmail.com>
2025-05-03 21:51:51 -0300

    feat(Backend tabela users): Criando model da tabela users

---
+++ app/backend/src/models/userModel.js
@@ -0,0 +1,48 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    mobile: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    departament: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    verificationCode: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    emailChecked: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    cashforceAdm: { //tinyint(1) DEFAULT 0
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  }, {
+    tableName: 'users',
+    timestamps: true,
+  });
+
+
+  return User;
+};
